2e0e1e910bc1a9246929a534e98285b2ad2309b4
AlvinJohnB <70368809+AlvinJohnB@users.noreply.github.com>
2022-09-07 11:27:25 +0800

    commit-respersect

--- client/src/components/results/ResultForm.js
+++ client/src/components/results/ResultForm.js
@@ -10,253 +10,174 @@ Font.register({ family: 'arial', src: arial, fontStyle: 'normal', fontWeight: 'n
 
 // Create styles
 const styles = StyleSheet.create({
-  page: {
-    paddingTop: 15,
-    paddingBottom: 100,
+  body: {
+    paddingTop: 35,
+    paddingBottom: 460,
     paddingHorizontal: 35,
-    alignItems: 'center'
   },
-header:{
-    width: 575.28,
-    height: 120,
-    marginBottom: "5px"
-  },
-  wrap:{
-    width: 575.28,
-  },
-companyHeader:{
-    width: 575.28,
-    marginBottom: "10px"
-  },
-  companyHText:{
-    fontSize: '16px',
-    textAlign: 'center'
-  },
-  contactText:{
+  header:{
     textAlign: 'center',
-    fontSize: '8px'
+    marginTop: 15
   },
-  marginTop:{
-    marginTop: 2,
+  companyText:{
+    fontSize: 18,
+    fontFamily: 'Helvetica-Bold'
+  },
+  companyContacts:{
+    fontSize: 10
   },
   patientHeader:{
-    flex: 1,
-    flexDirection: 'row',
-    width: 575.28,
-    paddingLeft: '10px',
-  },
-  pcol:{
-    width: 350,
-  },
-  pcol1:{
-    width: 200,
-  },
-  patientInfoText:{
-    fontSize: '10px',
-  },
-  resultBody:{
-    width: 575.28,
-    paddingLeft: '10px',
-  },
-  resultHeader:{
-    fontFamily: 'Helvetica-Bold',
-    fontSize: '11px',
-    fontWeight: 'bold',
-    width: 575.28
-  },
-  footer:{
-    width: 575.28,
-    flex: 1,
+    display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-around',
-    position: 'absolute',
-    bottom: 29,
+    marginTop: 10
   },
-  footerCol:{
-    width: 200,
-    justifyContent: 'center',
-    alignItems: 'center',
+  column:{
+    width: 300,
+    marginRight: 20
   },
-  footerCol1:{
-    width: 200,
-    height: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
+  column1:{
+    width: 200
+  },
+  patientInfo:{
+    fontSize: '11px'
   },
   footerText:{
-    fontSize: '9px',
+    fontSize: '10px',
     textAlign: 'center',
-    fontWeight: 'bold',
-  },
-  footerMessage:{
+  }
+  ,
+  footer:{
     position: 'absolute',
-    fontSize: '9px',
-    textAlign: 'center',
-    fontWeight: 'bold',
-    bottom: 29,
-  },
-  resTable:{
-    width: 555.28,
-  },
-  resHText:{
-    fontFamily: 'Helvetica-Bold',
-    fontSize: '10px',
-    width: 130,
-    textAlign: 'center',
-    borderBottom: '1px dotted black',
-    borderTop: '1px dotted black',
-    padding: 5
-  },
-  testNameHeader:{
-    fontFamily: 'Helvetica-Bold',
-    fontSize: '10px',
-    fontWeight: 'bold',
-    width: 200,
-    textAlign: 'center',
-    borderBottom: '1px dotted black',
-    borderTop: '1px dotted black',
-    padding: 5
-  },
-  tableHeader:{
-    width: 560.28,
+    bottom: 400,
+    display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
   },
-  resTr:{
-    width: 560.28,
+  
+  resultHeader:{
+    borderTop: '1px dotted black',
+    borderBottom: '1px dotted black',
+    marginTop: 15,
+    marginBottom: 10,
+    display: 'flex',
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: "2px",
-    marginBottom: "2px"
+    justifyContent: 'space-around',
+    padding: 5,
+    fontFamily: 'Helvetica-Bold',
+    fontSize: '12px'
   },
-   body:{
-    paddingTop: 35,
-    paddingBottom: 65,
-    paddingHorizontal: 35,
+  
+   resultBody:{
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+     fontSize: '10px'
+  },
+  testName: {
+    width: 175,
+    paddingLeft: 15
+  },
+  resultText:{
+    width: 215,
+    textAlign: 'center',
+  },
+  unitText:{
+    width: 75,
+    textAlign: 'center'
 },
-testName:{
-  fontSize: '10px',
-  width: 170,
-  padding: 2
-},
-trCenter:{
-  fontSize: '10px',
-  width: 130,
-  padding: 2,
-  textAlign: 'center',
-},
-trCenterBold:{
-  fontFamily: 'Helvetica-Bold',
-  fontSize: '11px',
-  width: 200,
-  padding: 2,
-  textAlign: 'center',
-},
-logo:{
-  width: "55px",
-  position: 'absolute',
-  left: "40px"
-},
-lablogo:{
-  width: "55px",
-  position: 'absolute',
-  right: "40px",
-},
-signature:{
-  fontFamily: 'arial',
-  fontSize: '10px',
-},
-signaturePatho:{
-  fontFamily: 'arial',
-  fontSize: '10px',
-},
-result:{
-  width: 300
-}
+  referenceText:{
+    width: 75,
+    textAlign: 'center'
+  },
+  sectiontext:{
+    fontFamily: 'Helvetica-Bold',
+    fontSize: '11px'
+  },
 });
 
 // Create Document Component
 const ResultForm = (props) => {
   return(
-  <Document>
-    <Page size="A5" orientation= "landscape" style={styles.page}>
-      <View style={styles.wrap}>
-        <View fixed={true} style={styles.header}>
-          <View style={styles.companyHeader}>
-            <Image src={logo} style={styles.logo} fixed={true}/>
-            <Image src={lablogo} style={styles.lablogo} fixed={true}/>
-            <Text style={styles.companyHText}>St. Camillus De Lellis General Hospital</Text>
-            <Text style={styles.companyHText}>Laboratory Department</Text>
-            <Text style={styles.contactText}>Gomburza cor. Z. Flores Sts. Brgy. 6 San Agustin, Laoag City</Text>
-            <Text style={styles.contactText}>Cellphone No.: 0961 366 8271 | Telephone No.: 600-1125  |  e-mail: st.camillusdelellislab@yahoo.com</Text>
-            <Text style={[styles.companyHText, styles.marginTop]}>Laboratory Report</Text>
-          </View>
-          <View style={styles.patientHeader}>
-            <View style={styles.pcol}>
-              <Text style={styles.patientInfoText}>Patient Name: {props.data.Patientlists[0].lastname}, {props.data.Patientlists[0].firstname} {props.data.Patientlists[0].middlename}</Text>
-              <Text style={styles.patientInfoText}>Age/Gender: {props.data.Patientlists[0].age} / {props.data.Patientlists[0].gender}</Text>
-              <Text style={styles.patientInfoText}>Requesting Physician: {props.data.reqDr}</Text>
-            </View>
-
-            <View style={styles.pcol1}>
-              <Text style={styles.patientInfoText}>Date: {Moment(props.data.createdAt).format('MMMM DD, yyyy')}</Text>
-              <Text style={styles.patientInfoText}>Patient Type/Room: {props.data.ptType}</Text>
-              <Text style={styles.patientInfoText}>Laboratory Number: {props.data.labNumber}</Text>
-            </View>
-          </View>
+    <Document>
+    <Page size="Letter" style={styles.body}>
+    <View style={styles.header} fixed={true}>
+        <Image src={logo} style={styles.logo}/>
+        <Image src={lablogo} style={styles.lablogo}/>
+        <Text style={styles.companyText}>St. Camillus De Lellis General Hospital</Text>
+        <Text style={styles.companyContacts}>Gomburza cor. Z. Flores Sts. Brgy. 6 San Agustin, Laoag City</Text>
+        <Text style={styles.companyContacts}>Cellphone No.: 0961 366 8271 | Telephone No.: 600-1125  |  e-mail: st.camillusdelellislab@yahoo.com</Text>
+        <Text style={styles.companyText}>Laboratory Report</Text>   
+    </View>
+      
+      <View style={styles.patientHeader} fixed={true}>
+        <View style={styles.column}>
+          <Text style={styles.patientInfo}>Patient Name: {props.data.Patientlists[0].lastname}, {props.data.Patientlists[0].firstname} {props.data.Patientlists[0].middlename}</Text>
+          <Text style={styles.patientInfo}>Age/Gender: {props.data.Patientlists[0].age} / {props.data.Patientlists[0].gender}</Text>
+          <Text style={styles.patientInfo}>Requesting Physician: {props.data.reqDr}</Text>
         </View>
-
-        <View style={styles.resultBody}>
-          <Text fixed={true} style={styles.resultHeader}>Section: {props.data.Sectionorders[0].section}</Text>
-          
-          <View style={styles.resTable}>
-            
-            <View style={styles.tableHeader} fixed={true}>
-                <Text style={styles.testNameHeader}>Test Name</Text>
-                <Text style={[styles.resHText, styles.result]}>Result</Text>
-                <Text style={styles.resHText}>Unit</Text>
-                <Text style={styles.resHText}>Reference</Text>
-            </View>
-            {props.data.Sectionorders[0].Sectionresults.map((result, index) => {
-                return(
-                    <View key={index}>
-                    {result.result === "!" || result.result === null ?  <View></View> : <View wrap={false} style={styles.resTr}>
-                     {result.Testslist.isPackage === true && <Text style={styles.trCenterBold}>{result.Testslist.testname}</Text>}
-                     {result.Testslist.isPackage === false && <Text style={styles.testName}>{result.Testslist.testname}</Text>}
-                    <Text style={[styles.trCenter, styles.result]}>{result.result}</Text>
-                    {result.Testslist.isPackage === true && <Text style={styles.trCenter}></Text>}
-                    {result.Testslist.isPackage === false && <Text style={styles.trCenter}>{result.Testslist.unit}</Text>}
-                    {result.Testslist.Referencevalue == null &&  <Text style={styles.trCenter}></Text>}
-                    {result.Testslist.Referencevalue !== null && props.data.Patientlists[0].gender === "Male" && <Text style={styles.trCenter}>{result.Testslist.Referencevalue.Male}</Text>}
-                    {result.Testslist.Referencevalue !== null && props.data.Patientlists[0].gender === "Female" && <Text style={styles.trCenter}>{result.Testslist.Referencevalue.Female}</Text>}
-                </View>}
-                    </View>)
-            })}
-
-          </View>
-          
+        
+        <View style={styles.column1}>
+          <Text style={styles.patientInfo}>Date: {Moment(props.data.createdAt).format('MMMM DD, yyyy')}</Text>
+          <Text style={styles.patientInfo}>Patient Type/Room No.: {props.data.ptType}</Text>
+          <Text style={styles.patientInfo}>Laboratory Number: {props.data.labNumber}</Text>
         </View>
+      </View>
       
       
+      <View style={styles.resultHeader} fixed={true}>
+          <Text style={styles.testName}>Test Name</Text>
+          <Text style={styles.resultText}>Result</Text>
+          <Text style={styles.unitText}>Unit</Text>
+         <Text style={styles.referenceText}>Reference</Text>
+      </View>
       
-      
-        </View>
-         <View fixed={true} style={styles.footer}>
-         <View style={styles.footerCol}>
-            {/* IMAGE HERE FOR RMT */}
-            <Text style={styles.signature} fixed={true}>{props.data.Sectionorders[0].releasedBy}</Text>
-            <Text style={styles.footerText}>REGISTERED MEDICAL TECHNOLOGIST</Text>
-            <Text style={styles.footerText}>License No.: ______</Text>
-          </View>
 
-          <View style={styles.footerCol1}>
-          <Text style={styles.signaturePatho} fixed={true}>{props.data.Sectionorders[0].pathologist}</Text>
+         
+         {props.data.Sectionorders.map((section, key) => {
+            return(
+                <View key={key}>
+
+                    <Text style={styles.sectiontext}>{section.section === "CM" ? `Clinical Microscopy` : section.section}</Text>
+                {section.Sectionresults.map((result, index) => {
+                    return(
+                        <View key={index}>
+                            {result.result === "!" || result.result === null ?  <View></View> : <View wrap={false}>
+                            {result.Testslist.isPackage === true && <View> <Text style={styles.sectiontext}>{result.Testslist.testname}</Text></View>}
+                            {result.Testslist.isPackage === false && <View style={styles.resultBody}>
+                                                                        <Text style={styles.testName}>{result.Testslist.testname}</Text>
+                                                                        <Text style={styles.resultText}>{result.result}</Text>
+                                                                        <Text style={styles.unitText}>{result.Testslist.unit}</Text>
+                                                                        <Text style={styles.referenceText}>{props.data.Patientlists[0].gender === "Male" ? `${result.Testslist.Referencevalue.Male}` : `${result.Testslist.Referencevalue.Female}`}</Text>
+                                                                    </View>}
+                            </View>}
+                        </View>
+                    )
+                })}
+                </View>
+            )
+         })}
+         
+     
+      
+      
+      
+      
+      <View style={styles.footer} fixed={true}>
+        <View style={styles.column}>
+          <Text style={styles.footerText}>{props.data.Sectionorders[0].releasedBy}</Text>
+          <Text style={styles.footerText}>REGISTERED MEDICAL TECHNOLOGIST</Text>
+          <Text style={styles.footerText}>License No.: _____</Text>
+        </View>
+        
+        <View style={styles.column}>
+          <Text style={styles.footerText}>{props.data.Sectionorders[0].pathologist}</Text>
           <Text style={styles.footerText}>ANATOMIC AND CLINICAL PATHOLOGIST</Text>
-          <Text style={styles.footerText}>License No.: 98717</Text>
-          </View>
+          <Text style={styles.footerText}>License No: 98717</Text>
         </View>
-    </Page>
+      </View>
       
+    </Page>
   </Document>
 )
 };
